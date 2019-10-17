04b6a807940d055d6f6b230c0c8b4be56ff2f03a
tis13 <47393558+tis13@users.noreply.github.com>
2019-10-17 17:27:28 +0000

    Add files via upload

--- SackOverflow/src/main/java/is/hi/hpv501/sack/SackOverflow/Services/Implementations/APIServiceImplementation.java
+++ SackOverflow/src/main/java/is/hi/hpv501/sack/SackOverflow/Services/Implementations/APIServiceImplementation.java
@@ -1,35 +1,66 @@
 package is.hi.hpv501.sack.SackOverflow.Services.Implementations;
 
 import is.hi.hpv501.sack.SackOverflow.Services.APIService;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 
 import java.io.IOException;
 
 public class APIServiceImplementation implements APIService {
 
-    private OkHttpClient client = new OkHttpClient();
-
-
-    @Override
-    public String getAllTeams() throws IOException {
-        String apiKey = "tehrjtr76vjg";
-        String teamUrl = "https://www.fantasyfootballnerd.com/service/nfl-teams/json/" + apiKey + "/";
-        Request request = new Request.Builder().url(teamUrl).build();
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+    public APIServiceImplementation() {
     }
 
     @Override
+    public String getAllTeams() throws IOException {
+       // String apiKey = "tehrjtr76vjg";
+        String teamUrl = "https://www.fantasyfootballnerd.com/service/nfl-teams/json/tehrjtr76vjg/";
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(teamUrl)
+                .build();
+        try{
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+        }
+        return "villa";
+     /*   Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                try {
+                    throw new IOException(e);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String resp = response.body().string();
+                if(response.isSuccessful()){
+
+                }
+            }
+        }); */
+    }
+
+  /*  @Override
     public String run(String url) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
-        try (Response response = client.newCall(request).execute()) {
+        try {
+            Response response = client.newCall(request).execute();
             return response.body().string();
         }
     }
+
+   */
+
 }
