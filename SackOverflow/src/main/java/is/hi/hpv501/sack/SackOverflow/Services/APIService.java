3878efd25ecd5e34ae49cf159f5d1fe02ab3b653
tis13 <47393558+tis13@users.noreply.github.com>
2019-10-18 13:39:29 +0000

    Update APIService.java

--- SackOverflow/src/main/java/is/hi/hpv501/sack/SackOverflow/Services/APIService.java
+++ SackOverflow/src/main/java/is/hi/hpv501/sack/SackOverflow/Services/APIService.java
@@ -1,15 +1,27 @@
 package is.hi.hpv501.sack.SackOverflow.Services;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 import java.io.IOException;
 
 public interface APIService {
     String getAllTeams() throws  IOException;
-
-  //  String run(String url) throws IOException;
+    
+    String getAllPlayers() throws  IOException;
+    
+    String getGamesByWeek(String date, String away, String home, int nr) throws  IOException;
+    
+    String getGamePlayByPlay(String date, String away, String home, int nr) throws  IOException;
+    
+    String getPlayerStats(String playerName) throws  IOException;
+        
+    String getPlayerStats(String playerName) throws  IOException;
+    
+    String getTeamStats(String teamName) throws  IOException;
+    
+    String getLatestUpdates() throws IOException;
+    
+    String getAllInjuredPlayers() throws IOException;
+    
+    String getInjuredPlayersByTeam(String teamName) throws IOException;
 }
 
 
