eed91c6c3b5a93339a7ee99da2c2e43b1a7a2923
alp16 <32820574+alp16@users.noreply.github.com>
2019-11-01 14:36:09 +0000

    Add files via upload

--- SackOverflow/src/main/java/is/hi/hpv501/sack/SackOverflow/Entities/Teams.java
+++ SackOverflow/src/main/java/is/hi/hpv501/sack/SackOverflow/Entities/Teams.java
@@ -1,44 +1,36 @@
 package is.hi.hpv501.sack.SackOverflow.Entities;
 
-public class Teams {
-    private String code;
-    private String fullName;
-    private String city;
-
-    public Teams(){ }
-    public Teams(String code, String fullName, String city) {
-        this.code = code;
-        this.fullName = fullName;
-        this.city = city;
+    public enum Teams {
+        ARI,
+        ATL,
+        BAL,
+        BUF,
+        CAR,
+        CHI,
+        CIN,
+        CLE,
+        DAL,
+        DEN,
+        DET,
+        GB,
+        HOU,
+        IND,
+        JAX,
+        KC,
+        MIA,
+        MIN,
+        NE,
+        NO,
+        NYG,
+        NYJ,
+        OAK,
+        PHI,
+        PIT,
+        SD,
+        SEA,
+        SF,
+        STL,
+        TB,
+        TEN,
+        WAS
     }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    @Override
-    public String toString() {
-        return  code + ", "+ fullName+ ", " + city;
-    }
-
-}
